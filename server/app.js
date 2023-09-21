bdd29060caba6c14dba323eaddd7d2a6032641e8
Roshan <shahroshan0720@gmail.com>
2023-09-21 11:20:55 +0530

    initial setup

---
+++ server/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+//CREATE
+app.post('/insert',(req,res)=>{
+
+});
+
+//READ
+app.get('/getAll',(req,res)=>{
+    console.log('test');
+});
+
+
+const port = 3000|| process.env.PORT;
+
+app.listen(port,()=>{
+    console.log(`Server listening at http://localhost:${port}`);
+});
